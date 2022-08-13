30a5b3a48e94a719066949d1d5036f1216be3788
Diego Oliveira <diego9123msi@gmail.com>
2022-08-13 18:03:53 -0300

    upload project

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,4 @@
-<script setup>
-useHead({
-  title: 'Diego Oliveira - Desenvolvedor Web Full Stack',
-  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
-  charset: 'utf-8',
-  meta: [
-    { name: 'description', content: 'Olá! E sou o Diego. Um jovem entusiasta por desenvolvimento de software e um gamer, nas horas vagas. Minha carreira de programação começou a um ano e meio, desde então venho me aperfeiçoando cada vez mais.' },
-    { name: 'theme-color', content: '#000000' },
-  ],
-})
-</script>
+
 <template>
   <div id="sections" class="relative">
     <button @click="prevSlide" v-if="count"
@@ -19,8 +9,8 @@ useHead({
           d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894l6-3z" />
       </svg>
     </button>
-    <swiper :direction="'vertical'" :navigation="true" :pagination="false" :modules="modules"
-      class="mySwiper" @swiper="onSwiper">
+    <swiper :direction="'vertical'" :navigation="true" :pagination="false" :modules="modules" class="mySwiper"
+      @swiper="onSwiper">
       <swiper-slide>
         <Home />
       </swiper-slide>
@@ -36,7 +26,7 @@ useHead({
     </swiper>
     <button @click="nextSlide"
       class="absolute btn-next text-white bottom-10 transition ease-in-out hover:scale-110 duration-300"
-      :data-tooltip="pages[count + 1]" v-if="pages.length-1 > count">
+      :data-tooltip="pages[count + 1]" v-if="pages.length - 1 > count">
       <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-8 h-8" viewBox="0 0 16 16">
         <path fill-rule="evenodd"
           d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z" />
@@ -76,6 +66,15 @@ export default {
     }
   },
   setup() {
+    useHead({
+      title: 'Diego Oliveira - Desenvolvedor Web Full Stack',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+      charset: 'utf-8',
+      meta: [
+        { name: 'description', content: 'Olá! E sou o Diego. Um jovem entusiasta por desenvolvimento de software e um gamer, nas horas vagas. Minha carreira de programação começou a um ano e meio, desde então venho me aperfeiçoando cada vez mais.' },
+        { name: 'theme-color', content: '#000000' },
+      ],
+    })
     return {
       modules: [Pagination],
     };
